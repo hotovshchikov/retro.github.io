af915908df59f7f2b350f2c11b0f4cae7b4dac1d
kuznietsov <i.kuzniets@gmail.com>
2017-07-03 21:45:33 +0300

    added modal

--- js/scripts.js
+++ js/scripts.js
@@ -17,3 +17,24 @@ close.addEventListener('click',function () {
 });
 
 // УДАЛИТЬ ПОТОМ
+
+var btnOpen = document.getElementById('circle-button-l');
+var btnClose = document.getElementById('close');
+var btnCancel = document.getElementById('btnCancel')
+var modal = document.getElementById('modal-container');
+var body = document.body;
+
+btnOpen.onclick = function () {
+  modal.style.display = 'block';
+  body.style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
+}
+
+btnClose.onclick = function () {
+  modal.style.display = 'none'
+  body.style.backgroundColor = 'rgba(0, 0, 0, 0)'
+}
+
+btnCancel.onclick = function () {
+  modal.style.display = 'none'
+  body.style.backgroundColor = 'rgba(0, 0, 0, 0)'
+}
